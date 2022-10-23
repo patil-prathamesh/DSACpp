0b5cb416c8b1dcfa595c58c91eb9bdab948a59dc
prathamesh patil <patilprathamesh696@gmail.com>
2022-10-23 23:19:54 +0530

    Arrays folder added

---
+++ Arrays/C.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(arr[i]<0)
+            arr[i] = 2;
+        else if(arr[i] > 0)
+            arr[i] = 1;
+        cout << arr[i] << " ";
+    }
+}
