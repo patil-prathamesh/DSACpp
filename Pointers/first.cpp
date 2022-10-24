357aae99387cd5b580cf786d3f26f5997b4bd427
prathamesh patil <patilprathamesh696@gmail.com>
2022-10-24 23:17:45 +0530

    pointers folder added

---
+++ Pointers/first.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int *p = NULL;
+    // cout << *p; 
+    int i = 5;
+    int *q = &i;
+    cout << q << endl;
+    cout << *q << endl;
+    int *p = &i;
+    cout << p << endl;
+    cout << *p;
+    // int num = 2;
+    // cout <<  &num << endl;
+    // int *ptr = &num;
+    // cout << ptr << endl;
+    // cout << *ptr;
+}
